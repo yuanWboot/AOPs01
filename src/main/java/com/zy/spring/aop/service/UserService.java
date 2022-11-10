057e4e06a7e29f0451cf956faebe66c6482d9e4c
zy <1661996964@qq.com>
2022-11-10 15:09:08 +0800

    XML配置AOP

---
+++ src/main/java/com/zy/spring/aop/service/UserService.java
@@ -0,0 +1,24 @@
+package com.zy.spring.aop.service;
+
+import com.zy.spring.aop.dao.UserDao;
+
+public class UserService {
+    private UserDao userDao;
+
+    public void createUser(){
+        System.out.println("执行创建用户逻辑");
+        userDao.insert();
+    }
+
+    public String generateRandomPassword(String type , Integer length){
+        System.out.println("按"+type+"方法生成"+length+"位随机密码");
+        return "zhangsan";
+    }
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+}
